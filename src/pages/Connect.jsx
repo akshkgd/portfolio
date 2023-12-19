b03d2a28b6ad9cde592d9a24a7de799294861ab6
Ashish Shukla <akshkgd@gmail.com>
2023-12-19 19:42:06 +0530

    v1

---
+++ src/pages/Connect.jsx
@@ -0,0 +1,26 @@
+
+function Connect(){
+    return(
+        <>
+        <div className="home-container">
+            <h2>Have a project or an idea? Let's Connect over a zoom call!</h2>
+            <p>I'm a frontend developer, optimist, and community builder. I currently work as the VP of Product at Vercel, where I help teach the Next.js logomarkNext.js community, an open-source web framework built with React.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, similique.</p>
+
+            <ul>
+                <li>Mention your idea/requirement</li>
+                <li>Keep the content crisp!</li>
+                <li>What help you need from me</li>
+                <li>Do you have any budget</li>
+                <li>Any thing else I should know before we connect!</li>
+            </ul>
+            <div className='connect-flex'>
+                <a href="" className="connect">Book a live Zoom call</a>
+                
+                
+            </div>
+        </div>
+        </>
+    )
+}
+export default Connect;
