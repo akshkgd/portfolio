2afb0d79046411543a64032aa25a510db649320d
Ashish Shukla <akshkgd@gmail.com>
2023-12-19 19:06:31 +0530

    v1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import profilePic from '../assets/ashish.png'
+function Home(){
+    return(
+        <>
+        <div className="home-container">
+            <img src={profilePic} className='user' alt="" />
+            <h2>Hey I am Ashish!</h2>
+            <p>I'm a frontend developer, optimist, and community builder. I currently work as the VP of Product at Vercel, where I help teach the Next.js logomarkNext.js community, an open-source web framework built with React.</p>
+            <div className='connect-flex'>
+                <a href="" className="connect">My Resume</a>
+                <a href="" className="connect">Follow Me on social media</a>
+                <a href="" className="connect">Let's talk!</a>
+                
+            </div>
+        </div>
+        </>
+    )
+}
+export default Home;
